7fab8e7b73c50421c4600ac386cabe81dc7706ce
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-10-04 23:10:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  botToken: <YOUR BOT TOKEN HERE>",
+  botToken: 1302054231:AAHrx00VT8GR3vfREhL9RR9-2QnA29WnVVA",
   parseMode: "HTML",
   apiBaseUrl: "https://snowfl.com/OIcObqNfqpHTDvLKWQDNRlzQPbtqRcoKhtlled/",
 };
